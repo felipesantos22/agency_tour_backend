630727dc0bd98aafffe6108a9294818997fc9e4a
felipesantos22 <felipevs2018@gmail.com>
2023-11-30 15:35:31 -0300

    feat: first commit

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    public function create(Request $request)
+    {
+        $card = Card::create($request->all());
+        return response()->json(['message' => 'Task criada com sucesso!', 'Task' => $card]);
+    }
+
+    public function index()
+    {
+        $card = Card::all();
+        return $card;
+    }
+
+    public function destroy($id)
+    {
+        $card = Card::find($id);
+        if (!$card) {
+            return response()->json(['message' => 'Task não existe!']);
+        }
+        $card->delete();
+        return response()->json(['message' => 'Task deletada!']);
+    }
+}
---
+++ app/Models/Card.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $table = 'card';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'content',
+        'image'
+    ];
+
+    public $timestamps = false;
+}
