57cd1ab5c484ad206128eb5e1473b704af86b211
felipesantos22 <felipevs2018@gmail.com>
2024-05-12 03:53:45 -0300

    change mysql to sqlite

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [CardController::class, 'indexWeb']);
 Route::post('/', [CardController::class, 'createWeb']);
 Route::delete('/{id}', [CardController::class, 'destroyWeb']);
+
+
